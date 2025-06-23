582d2154660db741f67c01adc0885585bf4bde96
Rennmann, Julius <julius.rennmann@feynsinn.de>
2025-06-23 06:55:31 +0200

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,64 +1,21 @@
 return {
   {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
     config = function()
       require("mason").setup()
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim"
-    },
-    config = function()
-      require("lspconfig")
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "hrsh7th/cmp-nvim-lsp"
-    },
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       require("mason-lspconfig").setup()
-      require("mason-lspconfig").setup_handlers {
-        function(server_name)
-          require("lspconfig")[server_name].setup({ capabilities = capabilities })
-        end,
-        ["lua_ls"] = function()
-          require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                runtime = {
-                  version = 'LuaJIT',
-                },
-                diagnostics = {
-                  globals = { 'vim', 'use', 'require' },
-                },
-                workspace = {
-                  library = vim.api.nvim_get_runtime_file("", true),
-                },
-                telemetry = {
-                  enable = false,
-                },
-              },
-            },
-          })
-        end,
-      }
     end
   },
   {
     "jay-babu/mason-null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      "williamboman/mason.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
+      "nvimtools/none-ls.nvim",
+      "mason-org/mason-lspconfig.nvim",
     },
     config = function()
       require("mason-null-ls").setup({
@@ -74,5 +31,5 @@ return {
         }
       })
     end,
-  }
+  },
 }
